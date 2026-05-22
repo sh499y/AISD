f6f747e04c97e131082baed1927b4cbbc04173a8
shaggy <pawlaczykblazej@gmail.com>
2026-05-22 16:27:23 +0200

    added bubble soer

--- Sorotwania/bubble_sort.py
+++ Sorotwania/bubble_sort.py
@@ -1,7 +1,21 @@
 def bubble_sort(lista):
+    n = len(lista)
+
+    #Przejdz przez akzdy element listy
+    for i in range(n):
+        swapped = False
+
+        for j in range(0, n-i-1):
+            if lista[j] > lista[j+1]:
+                lista[j], lista[j+1] = lista[j+1], lista[j]
+                swapped = True
+        if(swapped == False):
+            break
 
 
 
 
 if __name__ == '__main__':
     lista = [64, 34, 25, 12, 22, 11, 90]
+    bubble_sort(lista)
+    print(lista)
