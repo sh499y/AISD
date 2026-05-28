26ad5afbcea684f75defcb25a87af6a8ddf57300
shaggy <pawlaczykblazej@gmail.com>
2026-05-29 01:09:27 +0200

    quciksort running

--- Sorotwania/dziel_i_rzadz/quicksort.py
+++ Sorotwania/dziel_i_rzadz/quicksort.py
@@ -1,17 +1,23 @@
-def divide(lista, high):
-    if len(lista) == 1:
-        return 0
-    else:
-        pivot = high
-        for i in lista[:-1]:
-            if i <= pivot:
-                mniejsze.append(i)
-            else:
-                wieksze.append(i)
+def divide(lista):
+    if len(lista) <= 1:
+        return lista
+
+    pivot = lista[-1] #Ostatni element z listy jest zawzze moim pivotem
+    mniejsze = []
+    wieksze = []
+
+    #porownywanie
+    for i in lista[:-1]: #o jeden element mniej zeby nie brac pivot
+        if i <= pivot:
+            mniejsze.append(i)
+        else:
+            wieksze.append(i)
+
+    return divide(mniejsze) + [pivot] + divide(wieksze)
+
+
 
 if __name__ == '__main__':
 
     lista = [10, 7, 8, 9, 1, 5]
-    high = lista[-1]
-    mniejsze = []
-    wieksze = []
+    print(divide(lista))
